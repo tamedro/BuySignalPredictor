2d4563f98136e780be2402cd0f534aad57b03fd3
korytina <korytina@korytina-Inspiron-1545.(none)>
2015-05-03 20:46:04 -0600

    - added figure2:   baseline calculator

---
+++ figures/figure2/volatility_baseline_1year_ahead.py
@@ -0,0 +1,22 @@
+from numpy import mean
+from numpy import array
+
+import sklearn
+from sklearn import linear_model
+
+
+def read_csv(filename):
+    import csv
+    with open(filename) as f: rows=[tuple(row) for row in csv.reader(f)]
+    return rows
+    
+    
+# read in csv file
+rows = read_csv('ge-3.csv')
+
+#for ii in range(12000):
+for ii in range(12000):
+    predicted = float(rows[ii * 1 + 252][9])
+    actual    = float(rows[ii*1][9])
+    print ii*1,',', (actual-predicted)**2
+
